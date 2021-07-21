0e2ea7a9447bdc3b7c86f98a3b49d9d2b7021f5e
suman kumar <sumankumar7569@gmail.com>
2021-07-21 14:33:56 +0530

    video calling app using Jitsi API

---
+++ app/src/main/java/com/appdevelopersumankr/video_calling/MainActivity.java
@@ -0,0 +1,28 @@
+package com.appdevelopersumankr.video_calling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+
+        new Handler ().postDelayed( new Runnable() {
+            @Override
+            public void run() {
+               // startActivity(new Intent (MainActivity.this, LoginActivity.class));
+                Intent intent=new Intent (MainActivity.this,LoginActivity.class);
+                startActivity ( intent );
+                finish ();
+            }
+        },2000);
+
+    }
+}
